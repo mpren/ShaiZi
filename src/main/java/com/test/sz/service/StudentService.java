7dffc2cfcb56345fbcb488725dc7a5954c95c592
wangchenwen <mpren@qq.com>
2018-03-17 19:47:39 +0800

    更改路径名称

--- src/main/java/com/test/sz/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.test.sz.service;
-
-import com.test.sz.model.Student;
-
-public interface StudentService {
-
-    void insert(Student student);
-
-}
--- src/main/java/com/test/sz/service/TSInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.test.sz.service;
-
-import com.test.sz.model.TSInfo;
-
-public interface TSInfoService {
-
-    void insert(TSInfo tSInfo);
-
-}
