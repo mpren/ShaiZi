1a48d87e471784e16c9302b032ff9572bf4a9ad8
wangchenwen <mpren@qq.com>
2018-03-17 21:48:13 +0800

    添加注释

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # ShaiZi 自动化交易平台
 
-- 本项目依赖spring boot 2.0,netty,mybatis，mysql集成。
+## 本项目依赖spring boot 2.0,netty,mybatis,mysql,redis。
+
+- spring boot
+    - spring web
+- redis
+- mysql
 
--- src/main/java/com/mpren/sz/auto/InitDataLifecycle.java
+++ src/main/java/com/mpren/sz/auto/InitDataLifecycle.java
@@ -153,7 +153,7 @@ public class InitDataLifecycle implements SmartLifecycle {
                             if (null != str[47] && !"".equals(str[47]))
                                 s.setDieTingJia(BigDecimal.valueOf(Float.valueOf(str[47])));
 
-                            //系统设计规划使用kafka streams流式计算，但鉴于kafka比较重，当前使用Redis替代订阅发布。
+                            //系统设计规划使用kafka streams流式计算，但鉴于kafka比较重，当前使用Redis替代source/sink。
                             redisTemplate.convertAndSend("tsInfo", JSON.toJSONString(s));
                         }
                     }
