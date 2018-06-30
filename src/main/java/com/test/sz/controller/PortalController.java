19584ffc60c1effe3f7a923329ca94f65596f622
paxos <xxx@qq.com>
2018-06-30 11:00:32 +0800

    修改页面

--- src/main/java/com/test/sz/controller/PortalController.java
+++ src/main/java/com/test/sz/controller/PortalController.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.test.sz.business.PortalBusiness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -28,6 +27,5 @@ public class PortalController {
     public String queryTop5() {
         return JSON.toJSONString(portalBusiness.queryTop5());
     }
-
 
 }
